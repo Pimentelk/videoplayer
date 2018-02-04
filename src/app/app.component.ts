25d63fb7c37d103798ff6f3f9d9ce349eb2696d1
Kevin Pimentel <kevin@gymautopilot.com>
2018-02-03 22:05:00 -0500

    Migrated app to angular 5.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProgressComponent } from './progress.component';
+import { ToolbarComponent } from './toolbar.component';
+import { OptionsComponent } from './options.component';
+import { VideoService } from './video.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: '../assets/partials/videoplayer.html',
+  styleUrls: ['../assets/css/videoplayer.css'],
+  providers: [
+  	VideoService
+  ]
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+  
+  constructor(public videoService:VideoService) {}
+
+	ngOnInit() {
+		this.videoService.appSetup('videoDisplay');
+		this.videoService.gatherJSON();
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
+import { ToolbarComponent } from './toolbar.component';
+import { ProgressComponent } from './progress.component';
+import { OptionsComponent } from './options.component';
+import { TimeDisplayPipe } from './timedisplay.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    ProgressComponent,
+    OptionsComponent,
+    TimeDisplayPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+  	AppComponent
+  ]
 })
 export class AppModule { }
