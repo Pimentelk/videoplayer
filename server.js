7104945721516c6c6b96d15e064c30080462a001
Kevin Pimentel <kevin@gymautopilot.com>
2018-02-03 22:43:07 -0500

    Git commit fixed server js double declaration of express.

--- server.js
+++ server.js
@@ -7,6 +7,3 @@ app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
-
-const express = require('express');
-const app = express();
